2fa9ba01e84dc70528cd13c0bf1668f49bdc5baf
Matt Derrick <me@mattderrick.co.uk>
2016-11-21 21:38:36 +0000

    Throw error

--- scripts/deploy:demo.js
+++ scripts/deploy:demo.js
@@ -2,5 +2,7 @@ var ghpages = require('gh-pages');
 var path = require('path');
  
 ghpages.publish(path.join(__dirname, '../demo/dist'), function(err) {
-    console.error(err);
+	if (err) {
+    	throw err;
+	}
 });
