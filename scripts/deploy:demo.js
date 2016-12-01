508f3752350ef9e0bc3e6d0378db7062ffccbf89
Matt Derrick <me@mattderrick.co.uk>
2016-12-01 22:48:45 +0000

    demo errors

--- scripts/deploy:demo.js
+++ scripts/deploy:demo.js
@@ -1,8 +1,14 @@
-var ghpages = require('gh-pages');
-var path = require('path');
- 
-ghpages.publish(path.join(__dirname, '../demo/dist'), function(err) {
+/* eslint-disable no-console */
+
+const ghpages = require('gh-pages');
+const path = require('path');
+const chalk = require('chalk');
+
+ghpages.publish(path.join(__dirname, '../demo/dist'), (err) => {
     if (err) {
-        throw err;
+        console.log(chalk.red(err));
+        console.log(chalk.red('It is possible you have not built the demo. Please run "npm i && npm run i:demo && npm run build:demo" and try again.'));
+        return;
     }
+    console.log(chalk.green('Demo has succesfully deployed.'));
 });
