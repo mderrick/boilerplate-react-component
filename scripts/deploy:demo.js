ea1aeb4f17c85882b837256313bdf93811866120
Matt Derrick <me@mattderrick.co.uk>
2016-12-02 23:22:11 +0000

    Deploy demo update

--- scripts/deploy:demo.js
+++ scripts/deploy:demo.js
@@ -3,12 +3,27 @@
 const ghpages = require('gh-pages');
 const path = require('path');
 const chalk = require('chalk');
+const pkg = require('./../package.json');
+const fs = require('fs');
+const distPath = path.join(__dirname, '../demo/dist');
 
-ghpages.publish(path.join(__dirname, '../demo/dist'), (err) => {
-    if (err) {
-        console.log(chalk.red(err));
-        console.log(chalk.red('It is possible you have not built the demo. Please run "npm i && npm run i:demo && npm run build:demo" and try again.'));
-        return;
+try {
+    const author = pkg && pkg.author || {};
+    fs.accessSync(distPath, fs.F_OK);
+    if (!author.name || !author.email) {
+        console.log(chalk.red('Fields "author.name" and "author.email" are required in your package.json.'));
+    } else {
+        ghpages.publish(distPath, {
+            user: author.name,
+            email: author.email
+        }, (err) => {
+            if (err) {
+                console.log(chalk.red(err));
+                return;
+            }
+            console.log(chalk.green('Demo has succesfully deployed.'));
+        });
     }
-    console.log(chalk.green('Demo has succesfully deployed.'));
-});
+} catch (e) {
+    console.log(chalk.red('Please run "npm i && npm run i:demo && npm run build:demo" and try again.'));
+}
