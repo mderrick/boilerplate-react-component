32fec1d20c32c02981e4c013ef3709520e8dc563
Matt Derrick <me@mattderrick.co.uk>
2016-12-04 11:43:30 +0000

    Try and deploy gh pages

--- readme.md
+++ readme.md
@@ -12,9 +12,13 @@ You can view a demo [here](https://mderrick.github.io/boilerplate-react-componen
 
 ## Publishing
 
-Run `npm version <newversion>` as per the [npm docs](https://docs.npmjs.com/cli/version). This will create a tagged commit that is pushed to the repository.
+In order to publish and deploy your new package via TravisCI, you need to go through the following three steps.
 
-If you intend to deploy your package on npm via Travis you can modify the `.travis.yml` with your own details. The simplest way to do this is by following [this guide](https://docs.travis-ci.com/user/deployment/npm/) or running `travis setup npm`. TravisCI will pick up the tag, run tests, publish the package and deploy the demo to gh-pages branch.
+1. Modify the `.travis.yml` with your own npm details. The simplest way to do this is by following [this guide](https://docs.travis-ci.com/user/deployment/npm/) or running `travis setup npm`.
+
+2. To deploy the demo to a gh-pages branch you need to make a Github token with `repo` access on `process.env.GITHUB_TOKEN` or run `travis encrypt -r GITHUB_TOKEN=[the token here]` to add it to your travis.yml. Be warned if you have collaborators, this gives access to all repositories. Note: A project specific SSH key would prove safer. Look into adding this.
+
+Finally once the above has been set up, you can run `npm version <newversion>` as per the [npm docs](https://docs.npmjs.com/cli/version). This will create a tagged commit that is pushed to the repository and will start the TraviCI process deployment process.
 
 ## TODO
 - Install demo dependencies as post install
--- scripts/deploy:demo.js
+++ scripts/deploy:demo.js
@@ -8,21 +8,26 @@ const fs = require('fs');
 const distPath = path.join(__dirname, '../demo/dist');
 
 try {
-    const author = pkg && pkg.author || {};
-    fs.accessSync(distPath, fs.F_OK);
-    if (!author.name || !author.email) {
-        console.log(chalk.red('Fields "author.name" and "author.email" are required in your package.json.'));
+    if (process.env.GITHUB_TOKEN) {
+        const author = pkg && pkg.author || {};
+        fs.accessSync(distPath, fs.F_OK);
+        if (!author.name || !author.email) {
+            console.log(chalk.red('Fields "author.name" and "author.email" are required in your package.json.'));
+        } else {
+            ghpages.publish(distPath, {
+                repo: `https://${process.env.GITHUB_TOKEN}@$github.com/mderrick/webpack-react-boilerplate.git`,
+                user: author.name,
+                email: author.email
+            }, (err) => {
+                if (err) {
+                    console.log(chalk.red(err));
+                    return;
+                }
+                console.log(chalk.green('Demo has succesfully deployed.'));
+            });
+        }
     } else {
-        ghpages.publish(distPath, {
-            user: author.name,
-            email: author.email
-        }, (err) => {
-            if (err) {
-                console.log(chalk.red(err));
-                return;
-            }
-            console.log(chalk.green('Demo has succesfully deployed.'));
-        });
+        console.log(chalk.red('process.env.GITHUB_TOKEN with "repo" access is required to deploy gh-pages.'));
     }
 } catch (e) {
     console.log(chalk.red('Please run "npm i && npm run i:demo && npm run build:demo" and try again.'));
