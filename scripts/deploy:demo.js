fcdae99f788e9e1bd6ddbde138eaeae0ad5cd19d
Matt Derrick <me@mattderrick.co.uk>
2016-12-04 15:50:31 +0000

    demo deploy tidy

--- scripts/deploy:demo.js
+++ scripts/deploy:demo.js
@@ -1,34 +1,51 @@
 /* eslint-disable no-console */
 
 const ghpages = require('gh-pages');
+const pkg = require('./../package.json');
 const path = require('path');
 const chalk = require('chalk');
-const pkg = require('./../package.json');
 const fs = require('fs');
 const distPath = path.join(__dirname, '../demo/dist');
 
+const deploy = (options = {}) => {
+    ghpages.publish(distPath, Object.assign({
+        message: `v${pkg.version}`
+    }, options), (err) => {
+        if (err) {
+            error([err]);
+            return;
+        }
+        console.log(chalk.green('Demo has succesfully deployed.'));
+    });
+};
+
+const error = (errs = []) => {
+    errs.forEach((err) => {
+        console.log(chalk.red(err));
+    });
+    process.exit(1);
+};
+
 try {
-    if (process.env.GITHUB_TOKEN) {
-        const author = pkg && pkg.author || {};
-        fs.accessSync(distPath, fs.F_OK);
-        if (!author.name || !author.email) {
-            console.log(chalk.red('Fields "author.name" and "author.email" are required in your package.json.'));
-        } else {
-            ghpages.publish(distPath, {
-                repo: `https://${process.env.GITHUB_TOKEN}@github.com/mderrick/boilerplate-react-component.git`,
-                user: author.name,
-                email: author.email
-            }, (err) => {
-                if (err) {
-                    console.log(chalk.red(err));
-                    return;
+    fs.accessSync(distPath, fs.F_OK);
+    if (process.env.TRAVIS) {
+        if (process.env.GITHUB_TOKEN) {
+            deploy({
+                repo: `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.TRAVIS_REPO_SLUG}.git`,
+                user: {
+                    name: 'Travis CI'
                 }
-                console.log(chalk.green('Demo has succesfully deployed.'));
             });
+        } else {
+            error(['process.env.GITHUB_TOKEN with "repo" access is required to deploy gh-pages.']);
         }
     } else {
-        console.log(chalk.red('process.env.GITHUB_TOKEN with "repo" access is required to deploy gh-pages.'));
+        // Deploys using git origin, username and email.
+        deploy();
     }
 } catch (e) {
-    console.log(chalk.red('Please run "npm i && npm run i:demo && npm run build:demo" and try again.'));
+    error([
+        `${distPath} does not exist.`,
+        'Please run "npm i && npm run i:demo && npm run build:demo" and try again.'
+    ]);
 }
